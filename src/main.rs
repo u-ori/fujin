7ed19cb9caf9a98540645b1e686110b0fe5e6191
Ori <77636823+u-ori@users.noreply.github.com>
2023-12-05 01:51:08 +0000

    Single thread server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("bruh?");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {/
+        ("HTTP/1.1 200 OK", "static/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\nContent-Type: text/html\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
