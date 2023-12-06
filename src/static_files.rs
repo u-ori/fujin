f15f67771627ec7f158a10e8c99e9f527fcd2ca5
Ori <77636823+u-ori@users.noreply.github.com>
2023-12-06 21:12:06 +0000

    simplified the static files code

--- src/static_files.rs
+++ src/static_files.rs
@@ -1,4 +1,4 @@
-use http_body_util::{Full, StreamBody, combinators::BoxBody, BodyExt};
+use http_body_util::{StreamBody, combinators::BoxBody, BodyExt};
 use hyper::{Request, Response, Method, StatusCode};
 use hyper::body::{Bytes, Frame};
 use tokio_util::io::ReaderStream;
@@ -6,54 +6,24 @@ use tokio::fs::File;
 use futures_util::TryStreamExt;
 
 pub async fn serve(req: &Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, std::io::Error>>, std::io::Error>{
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") | (&Method::GET, "/index.html") => {
-            let file = File::open("static/index.html").await;
-            let file: File = file.unwrap();
-            let reader_stream = ReaderStream::new(file);
-            let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
-            let boxed_body = stream_body.boxed();
+    let filename = match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") | (&Method::GET, "/index.html") => "static/index.html",
+        (&Method::GET, "/debug") => "static/debug.html",
+        _ => "static/404.html",
+    };
+    
+    let file = File::open(filename).await;
+    let file: File = file.unwrap();
+    let reader_stream = ReaderStream::new(file);
+    let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
+    let boxed_body = stream_body.boxed();
 
-            // Send response
-            let response = Response::builder()
-                .header("Content-Type", "text/html")
-                .status(StatusCode::OK)
-                .body(boxed_body)
-                .unwrap();
+    // Send response
+    let response = Response::builder()
+        .header("Content-Type", "text/html")
+        .status(StatusCode::OK)
+        .body(boxed_body)
+        .unwrap();
 
-            Ok(response)
-        },
-        (&Method::GET, "/debug") => {
-            let file = File::open("static/debug.html").await;
-            let file: File = file.unwrap();
-            let reader_stream = ReaderStream::new(file);
-            let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
-            let boxed_body = stream_body.boxed();
-
-            // Send response
-            let response = Response::builder()
-                .header("Content-Type", "text/html")
-                .status(StatusCode::OK)
-                .body(boxed_body)
-                .unwrap();
-
-            Ok(response)
-        },
-        _ => {
-            let file = File::open("static/404.html").await;
-            let file: File = file.unwrap();
-            let reader_stream = ReaderStream::new(file);
-            let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
-            let boxed_body = stream_body.boxed();
-
-            // Send response
-            let response = Response::builder()
-                .header("Content-Type", "text/html")
-                .status(StatusCode::OK)
-                .body(boxed_body)
-                .unwrap();
-
-            Ok(response)
-        },
-    }
+    Ok(response)
 }
